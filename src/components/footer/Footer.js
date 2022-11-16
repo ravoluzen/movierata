4914d772c3082458390022514fcd3ed810229e58
Raj Shankar Tiwary <rajshankartiwary@gmail.com>
2022-11-16 13:22:02 +0530

    search bar css update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,8 +4,8 @@ import '../footer/Footer.scss'
 const Footer = () => {
   return (
     <div className='footer'>
-      <div>Moviereta</div>
-      <div>eeeeeeeeeeeeeee</div>
+      <div>Movierata</div>
+      <div>RAVOLUZEN</div>
     </div>
   )
 }
